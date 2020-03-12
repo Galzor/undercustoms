ecbfc846b603e01d316f8c45052ae7874fd94581
Galzor <binarymatter@gmail.com>
2020-03-12 22:00:50 +0530

    updated

--- template-parts/template-contact.php
+++ template-parts/template-contact.php
@@ -20,70 +20,58 @@ get_header();
       </div>
     </div>
 
-    <div class="content-block py-5">
+    <section class="common-section contact-section bg-white">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-6 text-center text-md-left">
+            <div class="section-title">
+              <p>Write to us </p>
+              <h2> <span>Let's talk</span> Get in Touch! </h2>
+            </div>
+            <p class="lead">Our team is available to talk with you, 24/7/365 right on moment's notice, just give it a shot. </p>
+            <!-- <p class="lead">Our background in financial mathematics and digital marketing, give us an analytical and quantitative approach to do just that. </p> -->
 
-      <section class="common-section contact-section py-3">
-        <div class="container">
-          <div class="card">
-            <div class="card-body">
-              <h4> Get in Touch </h4>
-              <?php echo do_shortcode('[contact-form-7 id="478" title="Contact form 1"]'); ?>
+            <div class="row mt-4">
+              <!-- <div class="col-md-12">
+                <i class="fa fa-map-marker fa-2x text-color"></i>
+                <h5 class="card-title mb-1 mt-2">Address</h5>
+                <?php // $address_mod = ( !empty(get_theme_mod('undercustoms_contact_info_address')) ) ? get_theme_mod('undercustoms_contact_info_address') : 'Set Address in theme options' ; ?>
+                <p class="card-text"> <?php // echo $address_mod; ?> </p>
+              </div> -->
+              <div class="col-md-6">
+                <i class="fa fa-phone fa-2x text-color"></i>
+                <h5 class="card-title mb-1 mt-2">Call us</h5>
+                <?php $phone_mod = ( !empty(get_theme_mod('undercustoms_contact_info_phone')) ) ? get_theme_mod('undercustoms_contact_info_phone') : 'Set Phone in theme options' ; ?>
+                <p class="card-text"> <a href="tel:<?php echo str_replace(' ', '', $phone_mod); ?>"><?php echo $phone_mod; ?></a> </p>
+              </div>
+              <div class="col-md-6">
+                <i class="fa fa-envelope fa-2x text-color"></i>
+                <h5 class="card-title mb-1 mt-2">Email</h5>
+                <?php $email_mod = ( !empty(get_theme_mod('undercustoms_contact_info_email')) ) ? get_theme_mod('undercustoms_contact_info_email') : 'Set Email in theme options' ; ?>
+                <p class="card-text"> <a href="mailto:<?php echo $email_mod; ?>"><?php echo $email_mod; ?></a> </p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-6 pt-4 pt-md-0 pl-md-5">
+            <div class="touch-form text-center text-md-left">
+              <h4 class="jesse-faden">Share your story!</h4>
+              <?php echo do_shortcode('[contact-form-7 id="536" title="Contact form"]'); ?>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <!--Services-->
-      <section class="common-section contact-section py-3">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-md-4">
-              <div class="card text-center">
-                <div class="card-body">
-                  <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
-                  <h5 class="card-title mb-1 mt-2">Address</h5>
-                  <?php $address_mod = ( !empty(get_theme_mod('undercustoms_contact_info_address')) ) ? get_theme_mod('undercustoms_contact_info_address') : 'Set Address in theme options' ; ?>
-                  <p class="card-text"> <?php echo $address_mod; ?> </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card text-center">
-                <div class="card-body">
-                  <i class="fa fa-phone fa-2x" aria-hidden="true"></i>
-                  <h5 class="card-title mb-1 mt-2">Phone</h5>
-                  <?php $phone_mod = ( !empty(get_theme_mod('undercustoms_contact_info_phone')) ) ? get_theme_mod('undercustoms_contact_info_phone') : 'Set Phone in theme options' ; ?>
-                  <p class="card-text"> <a href="tel:<?php echo str_replace(' ', '', $phone_mod); ?>"><?php echo $phone_mod; ?></a> </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card text-center">
-                <div class="card-body">
-                  <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
-                  <h5 class="card-title mb-1 mt-2">Email</h5>
-                  <?php $email_mod = ( !empty(get_theme_mod('undercustoms_contact_info_email')) ) ? get_theme_mod('undercustoms_contact_info_email') : 'Set Email in theme options' ; ?>
-                  <p class="card-text"> <a href="mailto:<?php echo $email_mod; ?>"><?php echo $email_mod; ?></a> </p>
-                </div>
-              </div>
-
-            </div>
+    <section class="common-section contact-section py-5">
+      <div class="container">
+        <div class="card card-body">
+          <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65337495.96929028!2d13.221284835372032!3d1.7840699917875809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3663f18a24cbe857%3A0xa9416bfcd3a0f459!2sAsia!5e0!3m2!1sen!2sin!4v1584025892218!5m2!1sen!2sin" frameborder="0" style="width: 100%; height: 400px; border:none; display:block;" allowfullscreen></iframe>
           </div>
         </div>
-      </section>
-
-      <section class="common-section contact-section py-3">
-        <div class="container">
-          <div class="card card-body">
-            <div class="map">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5132297.359442323!2d5.968357985600645!3d51.08992317063123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a721ec2b1be6b%3A0x75e85d6b8e91e55b!2sGermany!5e0!3m2!1sen!2sin!4v1580692788039!5m2!1sen!2sin" frameborder="0" style="width: 100%; height: 400px; border:none; display:block;" allowfullscreen></iframe>
-            </div>
-          </div>
-        </div>
-      </section>
-
-    </div>
+      </div>
+    </section>
 
   </main><!-- #main -->
 </div><!-- #primary -->
